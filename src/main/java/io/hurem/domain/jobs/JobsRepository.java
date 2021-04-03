fc9531ff71dc590a6df7deec9d344e6bbf6e2e08
Tim <iwasahorse@gmail.com>
2021-04-03 23:28:15 +0900

    컨트롤러, 컨트롤러 테스트 작성

--- src/main/java/io/hurem/domain/jobs/JobsRepository.java
+++ src/main/java/io/hurem/domain/jobs/JobsRepository.java
@@ -1,6 +1,5 @@
 package io.hurem.domain.jobs;
 
-import io.hurem.domain.employees.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobsRepository extends JpaRepository<Jobs, Long>  {
