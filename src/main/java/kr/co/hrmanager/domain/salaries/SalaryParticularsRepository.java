47552fe6f224646b98bff3e0da03c730fd489ef1
Tim <ridenowhere90@gmail.com>
2022-10-26 20:46:50 +0900

    급여 관련 Repository, Service 작성

---
+++ src/main/java/kr/co/hrmanager/domain/salaries/SalaryParticularsRepository.java
@@ -0,0 +1,6 @@
+package kr.co.hrmanager.domain.salaries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaryParticularsRepository extends JpaRepository<SalaryParticulars, Long> {
+}
---
+++ src/main/java/kr/co/hrmanager/service/salaries/SalarieyParticularsService.java
@@ -0,0 +1,27 @@
+package kr.co.hrmanager.service.salaries;
+
+import kr.co.hrmanager.domain.salaries.Salaries;
+import kr.co.hrmanager.domain.salaries.SalaryParticulars;
+import kr.co.hrmanager.domain.salaries.SalaryParticularsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class SalarieyParticularsService {
+    private final SalaryParticularsRepository repository;
+
+    public Optional<SalaryParticulars> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<SalaryParticulars> findByAll(Example<SalaryParticulars> queryParam) {
+        return repository.findAll(queryParam);
+    }
+}
