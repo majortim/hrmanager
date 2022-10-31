aa686129ab4e4ac5fa90e9ea250b51ffcbbdb447
Tim <ridenowhere90@gmail.com>
2022-10-31 19:40:29 +0900

    안 쓰는 import 제거

--- src/test/java/kr/co/hrmanager/domain/departments/DepartmentsRepositoryTest.java
+++ src/test/java/kr/co/hrmanager/domain/departments/DepartmentsRepositoryTest.java
@@ -3,18 +3,13 @@ package kr.co.hrmanager.domain.departments;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class DepartmentsRepositoryTest {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     DepartmentsRepository departmentsRepository;
