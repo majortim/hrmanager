80637d467a26d40f747fd443073a1ce9b7c36293
majortim <ridenowhere90@gmail.com>
2022-11-07 23:51:23 +0900

    근태 타입 추가(출근)

--- src/main/java/kr/co/hrmanager/domain/tna/TnaType.java
+++ src/main/java/kr/co/hrmanager/domain/tna/TnaType.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum TnaType {
+    WORK("출근"),
+    OUT_ON_BUSINESS("외근"),
     DAY("휴가"),
     MORNING("오전 반차"),
     AFTERNOON("오후 반차"),
-    MINUTE("시간 지정"),
+    MINUTE("시간 지정 휴가"),
     STEP_OUT("외출"),
-    OUT_ON_BUSINESS("외근"),
     EARLY_LEAVE("조퇴"),
     ABSENCE_WITHOUT_LEAVE("무단결근"),
     LEAVE_OF_ABSENCE("휴직")
