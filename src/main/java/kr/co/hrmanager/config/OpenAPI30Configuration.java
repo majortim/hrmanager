f217b9fef20e10b6683525e2e49bff735d666fe1
majortim <ridenowhere90@gmail.com>
2023-06-11 00:53:52 +0900

    Swagger 전역 설정 추가 - Swagger 테스트 리퀘스트에 Bearer Token 추가

--- src/main/java/kr/co/hrmanager/config/OpenAPI30Configuration.java
+++ src/main/java/kr/co/hrmanager/config/OpenAPI30Configuration.java
@@ -2,6 +2,10 @@ package kr.co.hrmanager.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,4 +15,19 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT",
         scheme = "bearer"
 )
-public class OpenAPI30Configuration {}
+public class OpenAPI30Configuration {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
