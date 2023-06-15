27e99dc5cc99980a4756097247cdff8cdc70b364
majortim <ridenowhere90@gmail.com>
2023-06-15 23:16:15 +0900

    로깅 설정 추가 - 리퀘스트 로깅 필터 추가(MDC 적용) - 프로필 설정에 따라 appender 적용

---
+++ src/main/java/kr/co/hrmanager/config/LoggingConfig.java
@@ -0,0 +1,37 @@
+package kr.co.hrmanager.config;
+
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter loggingFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter() {
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                MDC.put("traceId", UUID.randomUUID().toString());
+                super.doFilterInternal(request, response, filterChain);
+                MDC.remove("traceId");
+            }
+        };
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+
+}
